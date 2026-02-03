ce68cefcedfa2896f058b5ece78c599d465feb91
Antonin Januska <antonin@antjanus.com>
2026-02-03 15:01:31 -0700

    feat(01-03): implement command parser and parser module index
    - Command parser extracts name from filename
    - Parser module exports all parsing functions
    - All exports verified and working

---
+++ src/parser/index.ts
@@ -0,0 +1,3 @@
+export { parseSkillFile, type ParsedSkillContent } from './skill-parser.js';
+export { parseCommandFile, type ParsedCommandContent } from './command-parser.js';
+export { extractMarkdownReferences, concatenateFiles } from './markdown-concat.js';
